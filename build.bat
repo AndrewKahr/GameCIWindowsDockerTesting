ed3b7ee6294072a4d085aed70593b2901bfdd06a
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2021-12-25 23:59:31 -0800

    Testing support for UWP and tvOS

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 docker build ./base -t gameci/windows-base
 docker build ./hub -t gameci/windows-hub
-docker build ./editor --build-arg version="2020.3.24f1" --build-arg changeSet="79c78de19888" --build-arg module="windows-il2cpp" -t gameci/windows-editor
+docker build ./editor --build-arg version="2020.3.24f1" --build-arg changeSet="79c78de19888" --build-arg module="windows-il2cpp" -t gameci/windows-editor:il2cpp
+docker build ./editor --build-arg version="2020.3.24f1" --build-arg changeSet="79c78de19888" --build-arg module="universal-windows-platform" -t gameci/windows-editor:uwp
+docker build ./editor --build-arg version="2020.3.24f1" --build-arg changeSet="79c78de19888" --build-arg module="appletv" -t gameci/windows-editor:tvos
--- run.bat
+++ run.bat
@@ -8,10 +8,9 @@ echo Y| reg export "HKLM\SOFTWARE\WOW6432Node\Microsoft\Microsoft SDKs\Windows\v
 :: We mount our project, registry keys, output directory, and the files for VS build tools
 :: Assuming default installation locations for VS and Win 10 SDK
 docker run -it ^
---mount type=bind,source=%CD%\test-project,target=c:/workspace/test-project ^
---mount type=bind,source=%CD%\output,target=c:/workspace/output ^
+--mount type=bind,source=%CD%\workspace,target=c:/workspace ^
 --mount type=bind,source=%CD%\regkeys,target=c:/regkeys ^
 --mount type=bind,source="C:\Program Files (x86)\Microsoft Visual Studio",target="c:\Program Files (x86)\Microsoft Visual Studio" ^
 --mount type=bind,source="C:\Program Files (x86)\Windows Kits",target="C:\Program Files (x86)\Windows Kits" ^
 --mount type=bind,source="C:\ProgramData\Microsoft\VisualStudio",target="C:\ProgramData\Microsoft\VisualStudio" ^
---rm --env-file ./credentials.env --env-file ./variables.env gameci/windows-editor
+--rm --env-file ./credentials.env --env-file ./variables.env gameci/windows-editor:uwp
